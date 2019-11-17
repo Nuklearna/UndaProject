4c2058e1d18d0cac78c2a09aa9fad1f3d2f57d83
Nuklearna <n.swenda@gmail.com>
2019-11-17 19:31:58 +0100

    Initial commit with VueJS instance with Webpack, Vue router and, quick navigation and dummy text for placeholder (without css styling)

---
+++ vue-cli/src/components/Contact.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+    <h2>{{ msgContact }}</h2>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data () {
+    return {
+      msgContact: 'Contact'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
