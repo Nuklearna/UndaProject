2813ee2d8653c4fd92bf998dd2fa094435bd515d
Nuklearna <n.swenda@gmail.com>
2019-11-18 16:14:56 +0100

    Port changes and .gitignore file created

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 const contact = require('./routes/API/contact');
 app.use('/API/contact', contact);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 
 //listen on port
